4151b00e82648bb6d3531e28e245d636be2f16e8
jgraff <jgraff@salesforce.com>
2016-12-09 12:48:32 -0800

    Aspect modals

--- MultiTable/src/main.js
+++ MultiTable/src/main.js
@@ -15,6 +15,7 @@ const SampleUtils = require('./SampleUtils');
 const Utils = require('./Utils');
 
 require('../../../Helpers/multiline.js');
+const handlebars = require('handlebars-template-loader/runtime');
 
 const template = {
   awesome: require('./template/awesome.handlebars'),
@@ -23,6 +24,7 @@ const template = {
   sampleModal: require('./template/modal-sample.handlebars'),
   subjectGroup: require('./template/subject-group.handlebars'),
   subjectModal: require('./template/modal-subject.handlebars'),
+  aspectModal: require('./template/modal-aspect.handlebars'),
 };
 
 const RealtimeChangeHandler = require('./RealtimeChangeHandler');
@@ -40,6 +42,7 @@ let loading;
 let mt;
 let sampleModal;
 let subjectModal;
+let aspectModal;
 
 LENS.addEventListener('refocus.lens.load', () => {
   LENS.addEventListener('refocus.lens.hierarchyLoad', onHierarchyLoad);
@@ -76,11 +79,13 @@ LENS.addEventListener('refocus.lens.load', () => {
   LENS.insertAdjacentHTML('beforeend', template.awesome(conf.awesome));
   awesome = document.getElementById('awesome');
 
-  // Add the subject and sample modals.
+  // Add the subject, aspect, and sample modals.
   LENS.insertAdjacentHTML('beforeend', '<div id="modal-sample" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridModalLabel" aria-hidden="true">');
   sampleModal = document.getElementById('modal-sample');
   LENS.insertAdjacentHTML('beforeend', '<div id="modal-subject" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridModalLabel" aria-hidden="true">');
   subjectModal = document.getElementById('modal-subject');
+  LENS.insertAdjacentHTML('beforeend', '<div id="modal-aspect" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="gridModalLabel" aria-hidden="true">');
+  aspectModal = document.getElementById('modal-aspect');
 
   // Add the container which holds the tables.
   LENS.insertAdjacentHTML('beforeend', '<div id="multi-table-container" class="row"></div>');
@@ -168,6 +173,7 @@ function doDraw(evt) {
       mt.insertAdjacentHTML('beforeend', p.template);
       setSampleListeners(p.subjectGroup);
       setSubjectListeners(p.subjectGroup);
+      setAspectListeners(p.subjectGroup);
     });
   } else {
     awesome.removeAttribute('hidden');
@@ -194,8 +200,8 @@ function bindContentToModal(modal, modalTemplate, context, content) {
 function setSampleListeners(subjectGroup) {
   const samples = document.getElementById(subjectGroup.name)
     .querySelectorAll('.sample');
-  for (let i = 0; i < samples.length; i++) {
-    samples[i].addEventListener('click', (evt) => {
+  samples.forEach((sample) => {
+    sample.addEventListener('click', (evt) => {
       const s = subjectGroup.samples[evt.target.dataset.sampleId.toLowerCase()];
       s.updatedAtFormatted = moment.tz(s.updatedAt, moment.tz.guess()).format(conf.dateFormatString);
       s.statusChangedAtFormatted = moment.tz(s.statusChangedAt,
@@ -211,14 +217,14 @@ function setSampleListeners(subjectGroup) {
         conf.modal.sample, s);
       $('#modal-sample').modal(); // open the modal
     });
-  }
+  });
 } // setSampleListeners
 
 function setSubjectListeners(subjectGroup) {
   const subjects = document.getElementById(subjectGroup.name)
     .querySelectorAll('.subject');
-  for (let i = 0; i < subjects.length; i++) {
-    subjects[i].addEventListener('click', (evt) => {
+  subjects.forEach((subject) => {
+    subject.addEventListener('click', (evt) => {
       const s = evt.target.dataset.subjectId.toLowerCase() ===
         subjectGroup.name.toLowerCase() ? subjectGroup.self :
         subjectGroup.subjects[evt.target.dataset.subjectId.toLowerCase()];
@@ -235,5 +241,42 @@ function setSubjectListeners(subjectGroup) {
         conf.modal.subject, s);
       $('#modal-subject').modal(); // open the modal
     });
-  }
+  });
 } // setSubjectListeners
+
+function setAspectListeners(subjectGroup) {
+  const aspectElements = document.getElementById(subjectGroup.name)
+                                 .querySelectorAll('.aspect');
+  let aspectObjects = new Map();
+  let samples = Object.values(subjectGroup.samples);
+  samples.forEach((sample) => {
+    aspectObjects.set(sample.aspect.name, sample.aspect);
+  });
+
+  aspectElements.forEach((aspectElement) => {
+    aspectElement.addEventListener('click', (evt) => {
+
+      let aspectName = evt.target.innerHTML;
+      let aspect = aspectObjects.get(aspectName);
+
+      if (aspect.tags && aspect.tags.length > 1) {
+        aspect.tags.sort(Utils.sort);
+      }
+
+      bindContentToModal(aspectModal, template.aspectModal,
+        conf.modal.aspect, aspect);
+      $('#modal-aspect').modal(); // open the modal
+      
+    });
+  });
+} // setAspectListeners
+
+handlebars.registerHelper('flattenRange', function(range) {
+  if (range == null) {
+    return null;
+  } else if (range[0] == range[1]) {
+    return range[0];
+  } else {
+    return range[0] + ' - ' + range[1];
+  }
+});
